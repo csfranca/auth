1f065dde80bc567e69033135267a62cc7b1256f1
Camila Franca <camila.franca@kellogg.northwestern.edu>
2023-05-18 00:39:57 +0000

    Cryptographing password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,13 +10,13 @@ class SessionsController < ApplicationController
 
     # 2. if the user exists -> check if they know their password
     if @user != nil
-
-    # 3. if they know their password -> login is successful
       if @user["password"] == params["password"]
 
-    # 4. if the email does not exist or they do not know their password -> login fails
+    # 3. if they know their password -> login is successful
     flash["notice"] = "Welcome"
     redirect_to "/companies"  
+
+    # 4. if the email does not exist or they do not know their password -> login fails
   else 
     flash["notice"] = "Nope."
     redirect_to "/sessions/new"
