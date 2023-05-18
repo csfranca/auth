f2a0f2f6da24b15731c704c6cfff58998b84d4cd
Camila Franca <camila.franca@kellogg.northwestern.edu>
2023-05-18 02:29:30 +0000

    Final

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources "tasks"
   resources "users"
   resources "sessions"
-
-  # get "/", :controller => "sessions", :action => "new"
+  
+  get "/", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
+  # /logout
 end
